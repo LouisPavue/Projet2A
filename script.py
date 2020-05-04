3a2e966dadd781f5318c313cab0024f1be019052
LouisPavue <50333667+LouisPavue@users.noreply.github.com>
2020-05-04 15:13:00 +0200

    filtrage des données

--- script.py
+++ script.py
@@ -4,13 +4,26 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+import time, datetime
 # lecture des donnees
 df = pd.read_csv("data/01.csv", header =0)
 # nettoyage des donnees
 df = df.dropna()
-df.index = np.arange(len(df))
 
-  
+
+#df = df[df["callsign"].str.contains("ATN3407")]
+
+df.index = np.arange(len(df))
+#for i in tqdm(range(0, len(df))):
+    #df.iloc[:,0] = datetime.datetime.fromtimestamp(df.iloc[:,0]).utcnow()
+
+# https://germain-forestier.info/teaching/files/FD4/09-hierarchique.pdf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist , squareform
+from scipy.cluster.hierarchy import linkage , dendrogram
 
 print("END")
         
+
